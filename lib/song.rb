6be31182cd991e966d7bff964090cb888e62db7d
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-19 22:34:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,10 +23,10 @@ class Song
   end 
   
   def self.create_by_name(song_name)
-  # song = self.new
-  # song.name = song_name
-  # @@all << song
-  # song 
+  song = self.new
+  song.name = song_name
+  @@all << song
+  song 
   
   end
 
