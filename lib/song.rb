97f94ae20ad75508cd25b3a16d7997c5b47b2de8
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-19 22:12:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,22 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+  
+  def initialize(song)
+  
+  end
+  
   def self.all
     @@all
   end
 
   def save
     self.class.all << self
+  end
+  
+  def self.create 
+  song = self.create
+  @all << self.name  
   end
 
 end
