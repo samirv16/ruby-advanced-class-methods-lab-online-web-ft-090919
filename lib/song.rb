ca0657a04822fece3418d3c8634e19ce19322941
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-20 00:28:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,8 +48,8 @@ class Song
 
   def self.create_from_filename(filename)
     song = self.new
-    song.name = (name.split(" - ")[1].chomp(".mp3"))
-    song.artist_name = (name.split(" - ")[0])
+    song.name = (filename.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (filename.split(" - ")[0])
     @@all << song
     song
   end
