0f32b43c249cddf0622d810e6db3fe01c02c69f2
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-20 00:40:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,13 +40,6 @@ class Song
   def self.alphabetical
    @@all.sort_by(&:name) # @@all.sort_by { |song| song.name }
   end 
-
-  def self.create_from_filename(name)
-    song = self.new 
-    song.name = (name.split(" - ")[1].chomp(".mp3"))
-    song.artist_name = (name.split(" - ")[0])
-    song
-  end
   
   def self.create_from_filename(name)
   song = self.new
